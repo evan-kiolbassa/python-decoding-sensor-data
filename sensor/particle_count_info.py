29476cdaaf961e7c11dee8223260ef268cee7982
evan-kiolbassa <evankiolbassa@hotmail.com>
2022-03-09 21:25:59 -0600

    ParticleData class creation

---
+++ sensor/particle_count_info.py
@@ -0,0 +1,36 @@
+from house_info import HouseInfo
+from datetime import date
+
+class ParticleData(HouseInfo):
+    def _convert_data(self,data):
+        recs = []
+        for rec in data:
+            rec = float(rec)
+            recs.append(rec)
+        return recs
+
+    def get_data_by_area(self, rec_area=0):
+        recs =  super().get_data_by_area('particulate', rec_area)
+        return self._convert_data(recs)
+
+    def get_data_by_date(self, rec_date=date.today()):
+        recs = super().get_data_by_date("particulate", rec_date)
+        return self._convert_data(recs)
+
+    def get_data_concentrations(self, data):
+        particulate = {
+            'good' : 0,
+            'moderate' :0,
+            'bad' : 0
+        }
+
+        for rec in data:
+            if rec['particulate'] <= 50:
+                particulate['good'] += 1
+            elif rec['particulate'] <= 100 and rec['particulate'] > 50:
+                particulate['moderate'] += 1
+            else:
+                particulate['bad'] += 1
+        return particulate
+
+    
